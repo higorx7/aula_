bb5e35f74c303d0235d969cded3bf8871df19e87
higorx7 <131978326+higorx7@users.noreply.github.com>
2023-06-20 11:29:23 -0500

    fff
    frf

---
+++ .vscode/atividade32.js
@@ -0,0 +1,22 @@
+
+
+/*function somar(n1,n2) {
+    return n1 + n2
+}
+
+alert(somar(10,20))*/
+
+/*function BemVindo(){
+    alert('bem vindo ao sistema')
+}
+BemVindo()*/
+
+/*function subtrair(){
+    var numero1 = Number (prompt('digite um numero: '))
+    var numero2 = Number(prompt('digite outro numero; '))
+    var soma = numero1 - numero2
+    alert(soma)
+}
+
+subtrair()*/
+
