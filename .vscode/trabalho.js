8824224ae081b00b4f9691b816380043f5067b8d
higorx7 <131978326+higorx7@users.noreply.github.com>
2023-06-27 12:05:53 -0500

    bnb
    bb

---
+++ .vscode/trabalho.js
@@ -0,0 +1,24 @@
+alert("Bem Vindo ao Sistema")
+var nome = prompt("crie um nome de usuario")
+var nome = prompt("crie uma senha")
+
+alert("Usuário cadastrado com sucesso!")
+
+alert("È nescessario fazer o login")
+
+var usuario
+var senha2
+
+while(usuario == nome || senha2 == senha){
+
+var usuario = prompt("digite seu nome de usuario")
+var senha2 = ("digite sua senha")
+
+if (usuario !== nome || senha2 == senha){
+    alert("login aprovado")
+}
+
+else{
+    alert("erro! Usuário ou senha inválidados.")
+}
+}
